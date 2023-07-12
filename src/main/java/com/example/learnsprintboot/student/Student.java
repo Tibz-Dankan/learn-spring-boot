c82832844cb5167d02723823b2460e6b7d1b5d8f
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-07-12 19:32:53 +0300

    feat: add student repository for data access layer

--- src/main/java/com/example/learnsprintboot/student/Student.java
+++ src/main/java/com/example/learnsprintboot/student/Student.java
@@ -1,8 +1,20 @@
 package com.example.learnsprintboot.student;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName= "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
+
     private Long id;
     private String name;
     private String email;
@@ -14,6 +26,13 @@ public class Student {
 
     public Student(Long id, String name, String email, LocalDate dob, Integer age) {
         this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public Student(String name, String email, LocalDate dob, Integer age) {
         this.name = name;
         this.email = email;
         this.dob = dob;
