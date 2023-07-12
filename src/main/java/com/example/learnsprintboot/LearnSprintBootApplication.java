b5dbf3e37ecabb8f789290c9752f2a023b48e3e1
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-07-12 11:27:54 +0300

    student model controller and services in spring

--- src/main/java/com/example/learnsprintboot/LearnSprintBootApplication.java
+++ src/main/java/com/example/learnsprintboot/LearnSprintBootApplication.java
@@ -1,21 +1,20 @@
 package com.example.learnsprintboot;
 
+import com.example.learnsprintboot.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
 @SpringBootApplication
-@RestController
 public class LearnSprintBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearnSprintBootApplication.class, args);
-	}
-
-	@GetMapping
-	public String hello(){
-		return "Hello world";
 	}
 
 }
